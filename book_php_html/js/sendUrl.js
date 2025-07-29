f804d95cffeffa94cb291573cb22e4632731a9f3
DaXuanGuJi <541608265@qq.com>
2025-07-29 14:42:04 +0800

    Add files via upload

--- book_php_html/js/sendUrl.js
+++ book_php_html/js/sendUrl.js
@@ -1,19 +1,23 @@
 var sendExeUrl="http://127.0.0.1:17251/_github/book_file_server/";
 
 
+var sendExeUrlStatu='';
 function sendExeStatu() {
 	var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4) {
 			try {
 				if (xhr.status === 200) {
-				errtxt.innerHTML=xhr.responseText;
+				sendExeUrlStatu=errtxt.innerHTML=xhr.responseText;
 				} else {
-				errtxt.innerHTML="⛔⛔⛔⛔⛔服务器连接失败。。。";
+				sendExeUrlStatu=errtxt.innerHTML="⛔⛔⛔⛔⛔服务器连接失败。。。";
 				}
 			} catch (e) {
-				errtxt.innerHTML="⛔⛔⛔⛔⛔服务器连接失败。。。";
+				sendExeUrlStatu=errtxt.innerHTML="⛔⛔⛔⛔⛔服务器连接失败。。。";
 			}
+		}
+		else{
+			sendExeUrlStatu=errtxt.innerHTML="⛔⛔⛔⛔⛔服务器连接失败。。。";
 		}
 	}
 	xhr.open('GET',sendExeUrl+ "get.php?statu=code&t=" + new Date().getTime(),true);//true为异步
